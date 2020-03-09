519e3999ca7152a5d253d2e49c2f2d03c67e0d75
Mauricio Almanza <ma@iMacs-MA.local>
2020-03-09 18:20:14 -0500

    Update json.response format

--- app.js
+++ app.js
@@ -13,28 +13,59 @@ app.use(express.json());
 //Get user and password and verify
 app.use((req, res, next) => {
 
-    const auth = {username: 'user@example.com', password: 'Abcd1234'} // change this
-  
-    //Username and password from header
-    const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
-    const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
-  
-    // Verify username and password are set and correct
-    if (username === auth.username && password === auth.password) {
-      console.log("API Access granted")
-      return next()
-    }
-  
-    // Access denied...
-    res.status(401).send('Authentication required.') // custom message
+  //const auth = {username: process.env.USER || 'user@example.com', password: process.env.PASS || 'Abcd1234'}
+  const auth = { username: 'user@example.com', password: 'Abcd1234' }
+  //Username and password from header
+  const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
+  const [username, password] = Buffer.from(b64auth, 'base64').toString().split(':')
+
+  // Verify username and password are set and correct
+  if (username === auth.username && password === auth.password) {
+    console.log("API Access granted")
+    return next()
+  }
+
+  // Access denied...
+  var msg =''
+  if (req.headers.authorization == undefined) {
+    msg = 'Not authorized. Authentication is required and has not yet been provided'
+  }
+  else {
+    msg = 'Not authorized. Authentication is required and has failed'
+  }
+  console.error(`Error: ${msg}`)
+  res.status(401);
+  res.json({
+    'status': 'Error',
+    'message': msg
   })
-  
+})
+
 
 //Routes
-app.use(require('./routes/product.routes'));
+app.use('/api/products', require('./routes/product.routes'));
+
+//Invalid routes
+app.use((req, res) => {
+  // Invalid request
+  res.status(404);
+  res.json({
+    'status': 'Error',
+    'message': `Invalid request. Route 'HOST${req.url}' not found`
+  })
+});
+
+//error route
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    'status': 'Error',
+    'message': error.message
+  })
+})
 
 
 //Run server
 app.listen(app.get('port'), () => {
-    console.log('Server listening on port', app.get('port'))
+  console.log('Server listening on port', app.get('port'))
 });
